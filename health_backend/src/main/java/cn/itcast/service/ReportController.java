53b636e50d077bce6c1ba9cb3737ad975fe36b1c
zlzongla <zlzongla@gmail.com>
2020-02-21 21:03:49 +0800

    添加excel报表导出功能

--- health_backend/src/main/java/cn/itcast/service/ReportController.java
+++ health_backend/src/main/java/cn/itcast/service/ReportController.java
@@ -3,9 +3,18 @@ package cn.itcast.service;
 import cn.itcast.constant.MessageConstant;
 import cn.itcast.entity.Result;
 import com.alibaba.dubbo.config.annotation.Reference;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -68,20 +77,69 @@ public class ReportController {
             return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
         }
     }
+
+    @RequestMapping("/exportBusinessReport")
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            Map<String, Object> result = userService.getBusinessReportData();
+            String reportDate = (String) result.get("reportDate");
+            Integer todayNewMember = (Integer) result.get("todayNewMember");
+            Integer totalMember = (Integer) result.get("totalMember");
+            Integer thisWeekNewMember = (Integer) result.get("thisWeekNewMember");
+            Integer thisMonthNewMember = (Integer) result.get("thisMonthNewMember");
+            Integer todayOrderNumber = (Integer) result.get("todayOrderNumber");
+            Integer thisWeekOrderNumber = (Integer) result.get("thisWeekOrderNumber");
+            Integer thisMonthOrderNumber = (Integer) result.get("thisMonthOrderNumber");
+            Integer todayVisitsNumber = (Integer) result.get("todayVisitsNumber");
+            Integer thisWeekVisitsNumber = (Integer) result.get("thisWeekVisitsNumber");
+            Integer thisMonthVisitsNumber = (Integer) result.get("thisMonthVisitsNumber");
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+
+            //获取模板的绝对路径
+            String templateRealPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "report_template.xlsx";
+            //创建表格对象
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(new File(templateRealPath)));
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            XSSFRow row = sheet.getRow(2);
+            row.getCell(5).setCellValue(reportDate);
+            row = sheet.getRow(4);
+            row.getCell(5).setCellValue(todayNewMember);
+            row.getCell(7).setCellValue(totalMember);//总会员数
+            row = sheet.getRow(5);
+            row.getCell(5).setCellValue(thisWeekNewMember);//本周新增会员数
+            row.getCell(7).setCellValue(thisMonthNewMember);//本月新增会员数
+            row = sheet.getRow(7);
+            row.getCell(5).setCellValue(todayOrderNumber);//今日预约数
+            row.getCell(7).setCellValue(todayVisitsNumber);//今日到诊数
+            row = sheet.getRow(8);
+            row.getCell(5).setCellValue(thisWeekOrderNumber);//本周预约数
+            row.getCell(7).setCellValue(thisWeekVisitsNumber);//本周到诊数
+            row = sheet.getRow(9);
+            row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
+            row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
+
+            int rowNum = 12;
+            for (Map map : hotSetmeal) {
+                String name = (String) map.get("name");
+                Long setmeal_count = (Long) map.get("setmeal_count");
+                BigDecimal proportion = (BigDecimal) map.get("proportion");
+                row = sheet.getRow(rowNum ++);
+                row.getCell(4).setCellValue(name);//套餐名称
+                row.getCell(5).setCellValue(setmeal_count);//预约数量
+                row.getCell(6).setCellValue(proportion.doubleValue());//占比
+            }
+
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("content-Disposition","attachment;filename=report.xlsx");
+            workbook.write(out);
+            out.flush();
+            out.close();
+            workbook.close();
+            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_SUCCESS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- health_provider/src/main/java/cn/itcast/service/impl/UserServiceImpl.java
+++ health_provider/src/main/java/cn/itcast/service/impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.itcast.service.impl;
 
 import cn.itcast.dao.MemberDao;
+import cn.itcast.dao.OrderDao;
 import cn.itcast.dao.UserDao;
 import cn.itcast.pojo.User;
+import cn.itcast.service.OrderService;
 import cn.itcast.service.UserService;
 import cn.itcast.utils.DateUtils;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +24,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private MemberDao memberDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     public User findByUserName(String username) {
         User userByQuery = userDao.findByUserName(username);
@@ -44,10 +50,42 @@ public class UserServiceImpl implements UserService {
         String today = DateUtils.parseDate2String(DateUtils.getToday());
         String mondayDate = DateUtils.parseDate2String(DateUtils.getThisWeekMonday());
         String firstDay4ThisMonth = DateUtils.parseDate2String(DateUtils.getFirstDay4ThisMonth());
+        //今日新增会员
         Integer todayNewMember = memberDao.findMemberCountByDate(today);
-        Integer memberTotalCount = memberDao.findMemberTotalCount();
-        Integer memberCountAfterDate = memberDao.findMemberCountAfterDate(mondayDate);
-        Integer memberCountAfterDate1 = memberDao.findMemberCountAfterDate(firstDay4ThisMonth);
-        return null;
+        //会员总数
+        Integer totalMember = memberDao.findMemberTotalCount();
+        //本周新增会员
+        Integer thisWeekNewMember = memberDao.findMemberCountAfterDate(mondayDate);
+        //本月新增会员
+        Integer thisMonthNewMember = memberDao.findMemberCountAfterDate(firstDay4ThisMonth);
+        //今日预约人数
+        Integer todayOrderNumber = orderDao.findOrderCountByDate(today);
+        //本周预约人数
+        Integer thisWeekOrderNumber = orderDao.findOrderCountAfterDate(mondayDate);
+        //本月预约人数
+        Integer thisMonthOrderNumber = orderDao.findOrderCountAfterDate(firstDay4ThisMonth);
+        //今日到诊人数
+        Integer todayVisitNumber = orderDao.findVisitsCountByDate(today);
+        //本周到诊人数
+        Integer thisWeekVisitNumber = orderDao.findVisitsCountAfterDate(mondayDate);
+        //本月到诊人数
+        Integer thisMonthVisitNumber = orderDao.findVisitsCountAfterDate(firstDay4ThisMonth);
+
+        //热门套餐
+        List<Map> hotSetmeal = orderDao.findHotSetmeal();
+        Map<String,Object> result = new HashMap<>();
+        result.put("reportDate",today);
+        result.put("todayNewMember",todayNewMember);
+        result.put("totalMember",totalMember);
+        result.put("thisWeekNewMember",thisWeekNewMember);
+        result.put("thisMonthNewMember",thisMonthNewMember);
+        result.put("todayOrderNumber",todayOrderNumber);
+        result.put("thisWeekOrderNumber",thisWeekOrderNumber);
+        result.put("thisMonthOrderNumber",thisMonthOrderNumber);
+        result.put("todayVisitsNumber",todayVisitNumber);
+        result.put("thisWeekVisitsNumber",thisWeekVisitNumber);
+        result.put("thisMonthVisitsNumber",thisMonthVisitNumber);
+        result.put("hotSetmeal",hotSetmeal);
+        return result;
     }
 }
